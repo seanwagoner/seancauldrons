ee25bbc99bc435ada24d46ee876ee68b206a1d4b
Sean <seanwagoner@icloud.com>
2024-04-29 11:41:49 -0700

    inventory updates implemented..

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -39,11 +39,32 @@ def get_capacity_plan():
     Start with 1 capacity for 50 potions and 1 capacity for 10000 ml of potion. Each additional 
     capacity unit costs 1000 gold.
     """
+    with db.engine.begin() as connection:
+        gold = connection.execute(sqlalchemy.text("""
+            SELECT gold from global_inventory
+            """)).fetchone()[0]
+        curr_ml_capacity = connection.execute(sqlalchemy.text("""
+            SELECT ml_capacity from global_inventory
+            """)).fetchone()[0] // 10000
+        curr_potion_capacity = connection.execute(sqlalchemy.text("""
+            SELECT potion_capacity from global_inventory
+            """)).fetchone()[0] // 50
+        
+        
+        if gold >= 2100:
+            potion_capacity = 1
+            ml_capacity = 1
+        elif gold >= 1100:
+            if curr_ml_capacity >= curr_potion_capacity:
+                potion_capacity = 1
+            else:
+                ml_capacity = 1
+        
 
     return {
-        "potion_capacity": 0,
-        "ml_capacity": 0
-        }
+        "potion_capacity": potion_capacity,
+        "ml_capacity": ml_capacity
+    }
 
 class CapacityPurchase(BaseModel):
     potion_capacity: int
@@ -57,6 +78,18 @@ def deliver_capacity_plan(capacity_purchase : CapacityPurchase, order_id: int):
     capacity unit costs 1000 gold.
     """
 
-
+    gold_spent = 1000 * capacity_purchase.ml_capacity + capacity_purchase.potion_capacity
+    new_potion_capacity = capacity_purchase.potion_capacity * 50
+    new_ml_capacity = capacity_purchase.ml_capacity * 10000
+    
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("""
+                                           UPDATE global_inventory SET 
+                                           gold = gold - :gold_spent, 
+                                           ml_capacity = ml_capacity + :new_ml_capacity,
+                                           potion_capacity = potion_capacity + new_potion_capacity"""), 
+                                           {"gold_spent" : gold_spent},
+                                           {"new_ml_capacity" : new_ml_capacity},
+                                           {"new_potion_capacity" : new_potion_capacity})
 
     return "OK"
